30bded47b23844ddcbe55c59921da93ea6495ee2
Gregory Ling <0b0100011101101100@gmail.com>
2023-10-25 14:59:38 -0500

    Fixed uSockets dependency path

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const uSockets = b.dependencyInner("uSockets", "uSockets", @import("uSockets/build.zig"), .{});
+    const uSockets = b.dependency("uSockets", .{});
 
     const lib = b.addStaticLibrary(.{
         .name = "uWebSockets",
