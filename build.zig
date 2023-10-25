6ed4bc74be5a2dc015d57d24010410c672ecd3fa
Gregory Ling <0b0100011101101100@gmail.com>
2023-10-25 15:12:21 -0500

    Try 2

--- build.zig
+++ build.zig
@@ -17,8 +17,8 @@ pub fn build(b: *std.Build) !void {
     lib.addIncludePath(.{ .path = "src" });
 
     lib.linkLibrary(uSockets.artifact("uSockets"));
+    lib.installLibraryHeaders(uSockets.artifact("uSockets"));
 
-    lib.installLibraryHeaders(uSockets);
     lib.installHeader("capi/libuwebsockets.h", "libuwebsockets.h");
 
     b.installArtifact(lib);
